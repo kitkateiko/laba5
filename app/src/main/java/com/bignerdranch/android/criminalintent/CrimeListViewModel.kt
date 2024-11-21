0d1e965df1956306496c88e8479b6c04099d4f78
kitkateiko <tyrkov2003@mail.ru>
2024-11-21 21:44:05 +0700

    livedata

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -4,5 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
-    val crimes = crimeRepository.getCrimes()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
 }
